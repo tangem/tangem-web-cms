74d9d37adf891baa49fad16af15ed2f3e4071d59
Pavel Sidorov <sidorovpav@ya.ru>
2022-09-30 13:42:52 +0300

    JS-53 init cms

---
+++ src/api/tag/content-types/tag/lifecycles.js
@@ -0,0 +1,32 @@
+let slugify = require("slugify");
+const { ApplicationError } = require("@strapi/utils").errors;
+
+module.exports = {
+  async beforeCreate(event) {
+    await generateSlug(event);
+  },
+};
+
+const generateSlug = async (event) => {
+  const DEFAULT_LOCALE = "en";
+  const { data } = event.params;
+  const id = event.params?.where?.id ?? null;
+  const locale = !id ? "en" : await getLocale(id);
+
+  //Generate slug for en locale only
+  if (!data.slug && data.title && locale === DEFAULT_LOCALE) {
+    data.slug = slugify(data.title, { lower: true });
+  }
+
+  if (!data.slug) {
+    throw new ApplicationError("Slug is required!");
+  }
+};
+
+const getLocale = async (id) => {
+  const res = await strapi.service("api::tag.tag").findOne(id);
+
+  return res.locale;
+};
+
+
