65539605fa8d27ce89ca51f1a8b6cfc873ef8e6e
Alla Manshina <moralobsession@gmail.com>
2024-03-11 11:57:36 +0400

    fix schema and downgrade styled components

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,14 @@
-
 module.exports = ({ env }) => {
   return {
     upload: {
       config: {
-        provider: 'aws-s3',
+        provider: "aws-s3",
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
           params: {
-            Bucket: env('AWS_BUCKET'),
+            Bucket: env("AWS_BUCKET"),
           },
         },
         actionOptions: {
@@ -19,5 +18,5 @@ module.exports = ({ env }) => {
         },
       },
     },
-  }
-}
+  };
+};
