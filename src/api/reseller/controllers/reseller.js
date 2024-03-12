49445073b4dc2627e1eee72cec8571aca6f3b712
Alla Manshina <moralobsession@gmail.com>
2024-03-12 19:38:08 +0400

    add country and reseller colleaction types

---
+++ src/api/reseller/controllers/reseller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reseller controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::reseller.reseller');
