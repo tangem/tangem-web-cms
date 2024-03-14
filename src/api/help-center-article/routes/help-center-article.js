d4807aa04c62771fd05c6774408a3c9fa3e6f051
Alla Manshina <moralobsession@gmail.com>
2024-03-14 14:22:36 +0400

    add FAQ content types

---
+++ src/api/help-center-article/routes/help-center-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * help-center-article router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::help-center-article.help-center-article');
---
+++ src/api/help-center-article/services/help-center-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * help-center-article service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::help-center-article.help-center-article');
