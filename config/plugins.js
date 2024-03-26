0e06ae633d9d3bab4ad317553522351abcae01e2
Pavel Sidorov <2793795+sidorovpavel@users.noreply.github.com>
2024-03-26 13:55:35 +0300

    fix config s3

--- config/plugins.js
+++ config/plugins.js
@@ -9,8 +9,8 @@ module.exports = ({ env }) => {
             secretAccessKey: env('AWS_ACCESS_SECRET'),
             region: env('AWS_REGION'),
             params: {
-              // ACL: env('AWS_ACL', 'public-read'),
-              // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+              ACL: env('AWS_ACL', 'public-read'),
+              signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
               Bucket: env('AWS_BUCKET'),
             },
             actionOptions: {
@@ -18,6 +18,7 @@ module.exports = ({ env }) => {
               uploadStream: {},
               delete: {},
             },
+            s3ForcePathStyle: false,
           },
         },
       },
